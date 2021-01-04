dc3801913b2e475236a7b510f3ac9b7f77834e99
rafaelc <rafaelcouto@hotmail.com>
2021-01-04 19:43:31 -0300

    GET /products

---
+++ backend/src/main/java/com/rafaeldev/dsdeliver/resources/ProductResource.java
@@ -0,0 +1,26 @@
+package com.rafaeldev.dsdeliver.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaeldev.dsdeliver.dto.ProductDTO;
+import com.rafaeldev.dsdeliver.services.ProductService;
+
+@RestController
+@RequestMapping(value ="/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<ProductDTO>> findAll(){
+		List<ProductDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
