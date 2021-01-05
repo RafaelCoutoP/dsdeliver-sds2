d47b9653c494c2a92a7b3f9fea09a31bc9038c6f
rafaelc <rafaelcouto@hotmail.com>
2021-01-04 21:25:20 -0300

    POST /orders

--- backend/src/main/java/com/rafaeldev/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/rafaeldev/dsdeliver/entities/Order.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -29,7 +28,7 @@ public class Order implements Serializable {
 	private Instant moment;
 	private OrderStatus status;
 	
-	@ManyToMany(fetch = FetchType.EAGER)
+	@ManyToMany
 	@JoinTable(name = "tb_order_product",
 			joinColumns = @JoinColumn(name = "order_id"),
 			inverseJoinColumns = @JoinColumn(name = "product_id"))
